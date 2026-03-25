f9d443f46f59ee33aff6ab6fca8e376cdfb44659
Muthusamy J <muthusamymuthusamy5977@gmail.com>
2026-03-25 21:10:35 +0530

    Remove emoji from welcome message

---
+++ hello.py
@@ -0,0 +1,15 @@
+# My First Python Program
+
+name = input("Enter your name: ")
+
+print("Hello", name)
+print("Welcome to GitHub")
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print("It is an Even number")
+else:
+    print("It is an Odd number")
+
+print("Thank you!")
